faaa67c71c633da4c01891df5995bb1d3877e056
im-pingo <cczjp89@gmail.com>
2025-01-21 18:06:56 +0800

    update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -8,34 +8,74 @@ import (
 
 // Logger is a simple interface that can be implemented by any logging system
 type Logger interface {
-	Trace(msg string, args ...interface{})
-	Debug(msg string, args ...interface{})
-	Info(msg string, args ...interface{})
-	Warn(msg string, args ...interface{})
-	Error(msg string, args ...interface{})
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	WithFields(fields map[string]interface{}) Logger
+	WithField(key string, value interface{}) Logger
+	WithError(err error) Logger
 }
 
 type defaultLog struct {
 }
 
-func (l *defaultLog) Trace(msg string, args ...interface{}) {
-	fmt.Printf(msg, args...)
+func (l *defaultLog) Debug(args ...interface{}) {
+	fmt.Println(args...)
 }
 
-func (l *defaultLog) Debug(msg string, args ...interface{}) {
-	fmt.Printf(msg, args...)
+func (l *defaultLog) Info(args ...interface{}) {
+	fmt.Println(args...)
 }
 
-func (l *defaultLog) Info(msg string, args ...interface{}) {
-	fmt.Printf(msg, args...)
+func (l *defaultLog) Warn(args ...interface{}) {
+	fmt.Println(args...)
 }
 
-func (l *defaultLog) Warn(msg string, args ...interface{}) {
-	fmt.Printf(msg, args...)
+func (l *defaultLog) Error(args ...interface{}) {
+	fmt.Println(args...)
 }
 
-func (l *defaultLog) Error(msg string, args ...interface{}) {
-	fmt.Printf(msg, args...)
+func (l *defaultLog) Fatal(args ...interface{}) {
+	fmt.Println(args...)
+}
+
+func (l *defaultLog) Fatalf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func (l *defaultLog) Debugf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func (l *defaultLog) Infof(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func (l *defaultLog) Warnf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func (l *defaultLog) Errorf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func (l *defaultLog) WithFields(fields map[string]interface{}) Logger {
+	return l
+}
+
+func (l *defaultLog) WithField(key string, value interface{}) Logger {
+	return l
+}
+
+func (l *defaultLog) WithError(err error) Logger {
+	return l
 }
 
 // pionLogger implements pion's LeveledLogger interface using our Logger
@@ -43,20 +83,12 @@ type pionLogger struct {
 	logger Logger
 }
 
-func (l *pionLogger) Trace(msg string) {
-	l.logger.Trace(msg)
-}
-
-func (l *pionLogger) Tracef(format string, args ...interface{}) {
-	l.logger.Trace(format, args...)
-}
-
 func (l *pionLogger) Debug(msg string) {
 	l.logger.Debug(msg)
 }
 
 func (l *pionLogger) Debugf(format string, args ...interface{}) {
-	l.logger.Debug(format, args...)
+	l.logger.Debugf(format, args...)
 }
 
 func (l *pionLogger) Info(msg string) {
@@ -64,7 +96,7 @@ func (l *pionLogger) Info(msg string) {
 }
 
 func (l *pionLogger) Infof(format string, args ...interface{}) {
-	l.logger.Info(format, args...)
+	l.logger.Infof(format, args...)
 }
 
 func (l *pionLogger) Warn(msg string) {
@@ -72,7 +104,7 @@ func (l *pionLogger) Warn(msg string) {
 }
 
 func (l *pionLogger) Warnf(format string, args ...interface{}) {
-	l.logger.Warn(format, args...)
+	l.logger.Warnf(format, args...)
 }
 
 func (l *pionLogger) Error(msg string) {
@@ -80,7 +112,15 @@ func (l *pionLogger) Error(msg string) {
 }
 
 func (l *pionLogger) Errorf(format string, args ...interface{}) {
-	l.logger.Error(format, args...)
+	l.logger.Errorf(format, args...)
+}
+
+func (l *pionLogger) Trace(msg string) {
+	l.logger.Debug(msg)
+}
+
+func (l *pionLogger) Tracef(format string, args ...interface{}) {
+	l.logger.Debugf(format, args...)
 }
 
 // pionLoggerFactory implements pion's LoggerFactory interface
@@ -98,7 +138,7 @@ func NewPionLoggerFactory(logger Logger) logging.LoggerFactory {
 // NewLogger implements logging.LoggerFactory
 func (f *pionLoggerFactory) NewLogger(scope string) logging.LeveledLogger {
 	return &pionLogger{
-		logger: f.logger,
+		logger: f.logger.WithField("scope", scope),
 	}
 }
 
@@ -108,24 +148,24 @@ func SetDefaultLogger(logger Logger) {
 	defaultLogger = logger
 }
 
-func Info(msg string, args ...interface{}) {
-	defaultLogger.Info(msg, args...)
+func Info(args ...interface{}) {
+	defaultLogger.Info(args...)
 }
 
-func Warn(msg string, args ...interface{}) {
-	defaultLogger.Warn(msg, args...)
+func Warn(args ...interface{}) {
+	defaultLogger.Warn(args...)
 }
 
-func Error(msg string, args ...interface{}) {
-	defaultLogger.Error(msg, args...)
+func Error(args ...interface{}) {
+	defaultLogger.Error(args...)
 }
 
-func Debug(msg string, args ...interface{}) {
-	defaultLogger.Debug(msg, args...)
+func Debug(args ...interface{}) {
+	defaultLogger.Debug(args...)
 }
 
-func Trace(msg string, args ...interface{}) {
-	defaultLogger.Trace(msg, args...)
+func Trace(args ...interface{}) {
+	defaultLogger.Debug(args...)
 }
 
 func GetLogger() Logger {
